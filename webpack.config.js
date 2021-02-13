3e65a3f67776f3b1770c4e1ff9250d6f4e997b6a
Mick Annese <mickannese@gmail.com>
2021-02-13 15:47:23 -0500

    webpack, babel, react, server implemented

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const webpack = require('webpack');
+
+
+
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
+
+
+/*
+ * We've enabled HtmlWebpackPlugin for you! This generates a html
+ * page for you when you compile webpack, which will make you start
+ * developing and prototyping faster.
+ *
+ * https://github.com/jantimon/html-webpack-plugin
+ *
+ */
+
+module.exports = {
+  mode: 'development',
+
+  output: {
+    path: path.resolve(__dirname, 'public')
+  },
+
+  plugins: [new webpack.ProgressPlugin()],
+
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      include: [path.resolve(__dirname, 'src')],
+      loader: 'babel-loader'
+    }, {
+      test: /.css$/,
+
+      use: [{
+        loader: "style-loader"
+      }, {
+        loader: "css-loader",
+
+        options: {
+          sourceMap: true
+        }
+      }]
+    }]
+  },
+
+  devServer: {
+    open: true,
+    host: 'localhost'
+  }
+}
